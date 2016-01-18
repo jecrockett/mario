f65bcb47e14722c8c28381422ed70a5e092ae5c0
James Crockett <jecrockett@gmail.com>
2016-01-17 18:29:18 -0700

    Organize tweak and refactor

--- mario.rb
+++ mario.rb
@@ -2,15 +2,30 @@ require 'pry'
 
 module Mario
 
-  def self.build_steps(number, char="#")
-    pounds = (1..number).to_a
+  def self.build(number, char="#")
+    symbols = (1..number).to_a
     spaces = (0..number - 1).to_a
-    flag_count = [3, 2, 1]
-    flag_count.shift if number <= 5
+    flag = set_flag_size(number)
 
     loop do
-      puts "#{" " * spaces.pop + char * pounds.shift}     |#{(char * flag_count.pop) unless flag_count.empty?}"
-      break if pounds.empty?
+      puts "#{stairs(symbols, spaces, char) +
+              add_flag(char, flag)}"
+      break if symbols.empty?
     end
+    "Mario...for the win!"
+  end
+
+  def self.set_flag_size(num)
+    return [1]    if num < 4
+    return [2, 1] if num < 7
+    [3, 2, 1]
+  end
+
+  def self.stairs(symbols, spaces, char)
+    "#{" " * spaces.pop + char * symbols.shift}"
+  end
+
+  def self.add_flag(char, flag)
+    "     |#{(char * flag.pop) unless flag.empty?}"
   end
 end
